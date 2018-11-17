cf839dc293eb7e2c5c3320fbfce787c48ae9f470
Levkovich Polina <levkovich.polina@gmail.com>
2018-11-18 02:21:18 +0400

    программа, которая знает о тебе все

--- source/0103-WhatIsYourName/Program.cs
+++ source/0103-WhatIsYourName/Program.cs
@@ -11,7 +11,7 @@ namespace _0103_WhatIsYourName
 
             Console.Write("Напишите, пожалуйта, ваше имя: ");
             //Включаем залёный цвет
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             //Просим написать имя
             //Console.ReadLine считает имя после нажатия ENTER и сохранит имя в переменную 'name'
             var name = Console.ReadLine();
@@ -19,18 +19,27 @@ namespace _0103_WhatIsYourName
 
             Console.Write("Напишите, пожалуйта, вашу фамилию: ");
             //Включаем жёлтый цвет
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
             //Просим написать фамилию
             //Console.ReadLine считает фамилию после нажатия ENTER и сохранит имя в переменную 'surname'
             var surname = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Gray;
 
+            Console.Write("Напишите, пожалуйта, сколько вам лет: ");
+            //Включаем жёлтый цвет
+           Console.ForegroundColor = ConsoleColor.Blue;
+            //Просим написать фамилию
+            //Console.ReadLine считает фамилию после нажатия ENTER и сохранит имя в переменную 'surname'
+            var howOld = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
             //Печатаем, как вас зовут.
             //Вместо {0} напишится то, что хранится в 'name'
-            //Вместо {1} напишится то, что хранится в 'surname'
-            Console.Write("Вас зовут {0} {1}", name, surname);
-
-            Console.Read();
+           //Вместо {1} напишится то, что хранится в 'surname'
+             Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Вам {2} лет,вас зовут {1} {0}", name, surname, howOld);
+           
+             Console.Read();
         }
     }
 }
